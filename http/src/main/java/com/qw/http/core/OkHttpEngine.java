866ef614b22c2583142678d9afdb1f11618d38e0
qinwei <qinwei_it@163.com>
2019-09-05 10:15:48 +0800

    add okhttp engine impl

--- http/src/main/java/com/qw/http/core/OkHttpEngine.java
+++ http/src/main/java/com/qw/http/core/OkHttpEngine.java
@@ -1,11 +1,22 @@
 package com.qw.http.core;
 
-import com.qw.http.exception.HttpException;
+import android.text.TextUtils;
 
+import com.qw.http.RequestManager;
+import com.qw.http.exception.HttpException;
+import com.qw.http.log.HttpLog;
+import com.qw.http.utils.HttpConstants;
+
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
 
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
+import okhttp3.RequestBody;
 
 /**
  * Created by qinwei on 2019-06-15 17:28
@@ -14,35 +25,96 @@ import okhttp3.OkHttpClient;
 public class OkHttpEngine extends HttpEngine {
 
 
+    private okhttp3.Response okResponse;
+
     @Override
     protected Response get(Request request) throws HttpException {
         try {
             OkHttpClient client = new OkHttpClient();
-            okhttp3.Request okRequest = new okhttp3.Request.Builder()
-                    .url(request.url)
-                    .build();
-            okhttp3.Response okResponse = client.newCall(okRequest).execute();
+            okhttp3.Request.Builder builder = new okhttp3.Request.Builder()
+                    .url(request.url);
+            addHeaders(builder, request.headers);
+            okResponse = client.newCall(builder.build()).execute();
             Response response = new Response();
             response.code = okResponse.code();
             response.inputStream = okResponse.body().byteStream();
+            return response;
         } catch (IOException e) {
             throw new HttpException(HttpException.ErrorType.IO, e.getMessage());
         }
-        return null;
     }
 
     @Override
     protected Response post(Request request, OnProgressUpdateListener listener) throws HttpException {
-        return null;
+        try {
+            OkHttpClient client = new OkHttpClient();
+            okResponse = client.newCall(builderOkRequest(request)).execute();
+            Response response = new Response();
+            response.code = okResponse.code();
+            response.inputStream = okResponse.body().byteStream();
+            return response;
+        } catch (IOException e) {
+            throw new HttpException(HttpException.ErrorType.IO, e.getMessage());
+        }
+    }
+
+    private okhttp3.Request builderOkRequest(Request request) {
+        okhttp3.Request.Builder builder = new okhttp3.Request.Builder();
+        builder.url(request.url);
+        addHeaders(builder, request.headers);
+        if (request.uploadFile != null) {
+            MultipartBody.Builder mMultipartBodyBuilder = new MultipartBody.Builder();
+            mMultipartBodyBuilder.setType(MultipartBody.FORM);
+            mMultipartBodyBuilder.addFormDataPart("file",
+                    request.uploadFile.substring(request.uploadFile.lastIndexOf("/") + 1),
+                    RequestBody.create(MediaType.parse("application/octet-stream"), new File(request.uploadFile)));
+            builder.post(mMultipartBodyBuilder.build());
+        } else if (!TextUtils.isEmpty(request.postContent) && request.uploadFiles != null) {
+            String encrypt = RequestManager.getInstance().getConfig().getSafeInterface().encrypt(request.postContent);
+            HttpLog.d("加密后数据:" + encrypt);
+
+            MultipartBody.Builder mMultipartBodyBuilder = new MultipartBody.Builder();
+            mMultipartBodyBuilder.setType(MultipartBody.FORM);
+            for (FileEntity file : request.uploadFiles) {
+                mMultipartBodyBuilder.addFormDataPart("file", file.getFileName(),
+                        RequestBody.create(MediaType.parse(file.getFileType()),
+                                new File(file.getFilePath())));
+            }
+            mMultipartBodyBuilder.addFormDataPart("data", "data", RequestBody.create(HttpConstants.JSON, encrypt));
+            builder.post(mMultipartBodyBuilder.build());
+        } else if (!TextUtils.isEmpty(request.postContent)) {
+            String encrypt = RequestManager.getInstance().getConfig().getSafeInterface().encrypt(request.postContent);
+            HttpLog.d("加密后数据:" + encrypt);
+            RequestBody body = RequestBody.create(HttpConstants.JSON, encrypt);
+            builder.post(body);
+        } else if (request.parameters != null && request.parameters.size() > 0) {
+            MultipartBody.Builder mMultipartBodyBuilder = new MultipartBody.Builder();
+            mMultipartBodyBuilder.setType(MultipartBody.FORM);
+            for (Map.Entry<String, String> entry : request.parameters.entrySet()) {
+                mMultipartBodyBuilder.addFormDataPart(entry.getKey(), entry.getValue());
+                builder.post(mMultipartBodyBuilder.build());
+            }
+        }
+        return builder.build();
     }
 
     @Override
     protected void close() {
-
+        if (okResponse != null) {
+            okResponse.close();
+        }
     }
 
     @Override
     protected void write(Request request, OutputStream outputStream, OnProgressUpdateListener listener) throws HttpException {
 
+    }
+
+    private void addHeaders(okhttp3.Request.Builder builder, HashMap<String, String> headers) {
+        if (headers != null && headers.size() > 0) {
+            for (Map.Entry<String, String> entry : headers.entrySet()) {
+                builder.addHeader(entry.getKey(), entry.getValue());
+            }
+        }
     }
 }
--- http/src/main/java/com/qw/http/utils/HttpConstants.java
+++ http/src/main/java/com/qw/http/utils/HttpConstants.java
@@ -1,10 +1,14 @@
 package com.qw.http.utils;
 
+import okhttp3.MediaType;
+
 /**
  * Created by qinwei on 2017/6/8.
  */
 
 public class HttpConstants {
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    public static final MediaType MEDIA_TYPE_PNG = MediaType.parse("image/png");
     public static final int SUCCESS = 0;
     public static final int FAIL = 1;
     public static final int TIME_OUT = 5000;
